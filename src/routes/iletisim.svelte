3ece5a1104e3e2cfc38c3d59656fcda7663fb0a8
Ertan Kayalar <ertankayalar@gmail.com>
2022-01-23 19:28:04 +0300

    layout-update

---
+++ src/routes/iletisim.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Container from '$lib/components/container.svelte';
+	import PageHeader from '$lib/components/page-header.svelte';
+	import Breadcrumbs from '$lib/components/breadcrumbs.svelte';
+	import BreadcrumbItem from '$lib/components/breadcrumb-item.svelte';
+	import Row from '$lib/components/row.svelte';
+</script>
+
+<Breadcrumbs>
+	<BreadcrumbItem text="Ana Sayfa" url="/" />
+	<BreadcrumbItem text="İletişim" url="/iletisim" current />
+</Breadcrumbs>
+
+
+<PageHeader title="İletişim" layout="minimal" />
+
