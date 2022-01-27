c887b32244b0e967ac702321d24e8b1c1d6dbf91
Ertan Kayalar <ertankayalar@gmail.com>
2022-01-27 11:12:29 +0300

    mobilemenuactive-fix

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -4,49 +4,50 @@
 	import MegaMenu from '$lib/components/megamenu.svelte';
 	import SectionMenu from '$lib/components/megamenu-section.svelte';
 	import SectionMenuItem from '$lib/components/megamenu-section-item.svelte';
-	import MobileMenu from '$lib/components/mobile-menu/index.svelte'
+	import MobileMenu from '$lib/components/mobile-menu/index.svelte';
 
 	const Logo = '/logo.svg';
 	const layout = 'center';
 	export let siteTitle;
 	const center = true;
 	const standart = false;
+	let mobile = false;
+	let mobileMenuActive = false;
+
+	$: mobileMenuActive = mobile;
+
+
 
 	let menu;
-	let mobileMenuActive = false;
-	let mobile = false;
+
+	function clickHandler() {
+		console.log('clicked header mobile, mobileMenuActive', mobile, mobileMenuActive);
+		mobile = !mobile;
+	}
 
 	siteTitle = import.meta.env.VITE_SITE_TITLE;
 
-
-	let mobileMenu = [
+	let mobileMenuItems	 = [
 		{ title: 'Ana Sayfa', href: '/' },
-		{ title: 'Ürünler', href:'#', 
+		{
+			title: 'Ürünler',
+			href: '#',
 			subItems: [
-				{ title: 'Ürün Kategori 1', href:'/product-category' },
-				{ title: 'Ürün Kategori 2', href:'/product-category' }
-			]},
-		{ title: 'Ürünler 2', href:'#', 
+				{ title: 'Ürün Kategori 1', href: '/product-category' },
+				{ title: 'Ürün Kategori 2', href: '/product-category' }
+			]
+		},
+		{
+			title: 'Ürünler 2',
+			href: '#',
 			subItems: [
-				{ title: 'Ürün Kategori 3', href:'/product-category' },
-				{ title: 'Ürün Kategori 4', href:'/product-category' }
-			]},
+				{ title: 'Ürün Kategori 3', href: '/product-category' },
+				{ title: 'Ürün Kategori 4', href: '/product-category' }
+			]
+		},
 		{ title: 'Hakkımızda', href: '/hakkimizda' },
 		{ title: 'İletişim', href: '/iletisim' }
-	]
-
-	function clickHandler() {
-		console.log('clicked');
-		mobile = !mobile;
-
-		/*if (menu.current.classList.contains('hidden')) {*/
-		/*menu.current.classList.remove('hidden');*/
-		/*} else {*/
-		/*menu.current.classList.add('hidden');*/
-		/*}*/
-	}
-
-	$: mobileMenuActive = mobile;
+	];
 
 </script>
 
@@ -57,7 +58,7 @@
 		class:items-center={center == true}
 	>
 		<div class="flex items-center justify-center w-full py-5 xl:w-1/3 xl:justify-start xl:pl-2 ">
-			<div class="px-4 text-gray-600 cursor-pointer xl:hidden" id="burger" on:click={clickHandler}>
+<div class="px-4 text-gray-600 cursor-pointer xl:hidden" id="burger" on:click={() => mobileMenuActive = !mobileMenuActive }>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6"
@@ -98,31 +99,6 @@
 				<MegaMenuItem text="İletişim" url="/iletisim" />
 			</MegaMenu>
 		</div>
-		<!-- <div class={`backdrop xl:hidden`} class:hidden={!mobile} /> -->
-		<Backdrop mobile={mobileMenuActive} class="xl:hidden" />
-		<div
-			class="fixed top-0 left-0 z-50 flex w-10/12 xl:hidden h-screen py-10 pl-3 shadow-2xl mobilemenu"
-			class:hidden={!mobile}
-			class:block={mobile}
-		>
-			<span on:click={clickHandler} class="fixed text-secondary-100 top-2 right-3">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-8 w-8"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-				>
-					<path
-						fillRule="evenodd"
-						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-						clipRule="evenodd"
-					/>
-				</svg>
-			</span>
-<ul class="mobile-menu-item xl:hidden">
-<MobileMenu items={mobileMenu} />
-</ul>
-		</div>
+<MobileMenu items={mobileMenuItems} bind:mobileMenuActive={mobileMenuActive} />
 	</section>
 </header>
-
--- src/lib/components/mobile-menu/index.svelte
+++ src/lib/components/mobile-menu/index.svelte
@@ -1,34 +1,39 @@
 <script>
-	import DownArrow from './svg/down-arrow.svelte';
+	import MobileItem from './mobile-item.svelte';
+	import Backdrop from '$lib/components/backdrop.svelte';
+	/*import Close from './svg/close.svelte'*/
+
+	export let mobileMenuActive = false;
+	let mobile = false;
+
+	$: mobileMenuActive = mobile;
 
 	export let items = [];
-	export let collapsed = false;
-	let menuList = [];
 
-	function clickHandler(index) {
-		menuList[index].classList.toggle('hidden');
+	function clickHandler() {
+		console.log('clicked mobile', mobileMenuActive);
+		mobile = !mobile;
+
 	}
+
 </script>
 
-{#each items as item, i}
-	<li on:click={() => clickHandler(i)}>
-		{#if item.subItems}
-			<span class="text-lg flex items-center mobileListItem mobileSubMenu ">
-				{item.title}
-				<span class="down-arrow">
-					<DownArrow />
-				</span>
-			</span>
-			<ul class="{collapsed ? 'hidden' : ''} mobileSubMenuList " bind:this={menuList[i]}>
-				{#each item.subItems as subMenuItem}
-					<li><a href={subMenuItem.href}>{subMenuItem.title}</a></li>
-				{/each}
-			</ul>
-		{:else}
-			<a href={item.href}>{item.title}</a>
-		{/if}
-	</li>
-{/each}
-
-<style>
-</style>
+<Backdrop mobile={mobileMenuActive} class="xl:hidden" />
+<div
+	class="fixed top-0 left-0 z-50 flex w-10/12 xl:hidden h-screen py-10 pl-3 shadow-2xl mobilemenu"
+	class:hidden={!mobileMenuActive}
+	class:block={mobileMenuActive}
+>
+<span on:click={() => mobileMenuActive = !mobileMenuActive } class="fixed text-secondary-100 top-2 right-3 hover:cursor-pointer  p-5">
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+			<path
+				fillRule="evenodd"
+				d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+				clipRule="evenodd"
+			/>
+		</svg>
+	</span>
+	<ul class="mobile-menu-item xl:hidden">
+		<MobileItem {items} />
+	</ul>
+</div>
